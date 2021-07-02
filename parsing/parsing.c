1cdb3b46988438d29f56e9be4a834ac1ecf0bf81
Yuri Lee <ylee@c6r7s6.42seoul.kr>
2021-07-02 22:13:07 +0900

    add idx each char

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,51 +6,111 @@
 /*   By: inyang <inyang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:11:14 by inyang            #+#    #+#             */
-/*   Updated: 2021/07/02 20:11:50 by inyang           ###   ########.fr       */
+/*   Updated: 2021/07/02 22:07:27 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-int	main(int ac, char **av)
+int redir_name(char *line, int *changed, int i)
+{
+	if (line[i] == '<')
+	{
+		changed[i] = 6;
+	}
+	else if (line[i] == '>')
+	{
+		changed[i] = 7;
+	}
+	else
+		printf("error redir_name\n");
+	return (i);
+}
+
+int	env_name(char *line, int *changed, int i)
+{
+	changed[i] = 5;
+	while (line[++i])
+	{
+		if ((line[i] >= 'A' && line[i] <= 'Z') ||
+				(line[i] >= 'a' && line[i] <= 'z') ||
+				(line[i] >= '0' && line[i] <= '9') ||
+				(line[i] == '_'))
+			changed[i] = 5;
+		else
+			break ;
+	}
+	return (i - 1);
+}
+
+int	s_quote(char *line, int *changed, int i)
+{
+	changed[i] = 4;
+	while (line[++i])
+	{
+		if (line[i + 1] != '\'')
+			changed[i] = 1;
+		else if (line[i + 1] == '\'')
+		{
+			changed[i] = 1;
+			changed[i + 1] = 4;
+			i++;
+			break;
+		}
+	}
+	return (i);
+}
+
+int	d_quote(char *line, int *changed, int i)
+{
+	changed[i] = 3;
+	while (line[++i])
+	{
+		if (line[i] == '$')
+			i = env_name(line, changed, i);
+		else if (line[i + 1] != '\"')
+			changed[i] = 1;
+		else if (line[i + 1] == '\"')
+		{
+			changed[i] = 1;
+			changed[i + 1] = 3;
+			i++;
+			break;
+		}
+	}
+	return (i);
+}
+
+int	parsing(char *line)
 {
 	int		i;
-	int	*changed;
-	char	*line;
+	int		*changed;
 	int		length;
 
-	line = av[1];
 	length = px_strlen(line);
-	changed = ((int *)malloc(sizeof(int) * length));
+	changed = (int *)malloc(sizeof(int) * length);
 	i = 0;
 	while (i < length)
-		changed[i++] = 5;
+		changed[i++] = 1111111;
 	printf("%s\n", line);
 	printf("%d\n", length);
 	i = 0;
 	while (line[i])
 	{
-		printf("i = %d, %c\n", i, line[i]);
-		if (line[i] >= 'a' && line[i] <= 'z')
+		if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))
 			changed[i] = 0;
 		else if (line[i] == ' ' || line[i] == '\t')
 			changed[i] = 2;
-		else if (line[i] == 34)
-		{
-			changed[i] = 3;
-			while (line[++i])
-			{
-				if (line[i + 1] != 34)
-					changed[i] = 1;
-				else if (line[i + 1] == 34)
-				{
-					changed[i] = 1;
-					changed[i + 1] = 3;
-					i += 2;
-					break;
-				}
-			}
-		}
+		else if (line[i] == '\"')
+			i = d_quote(line, changed, i);
+		else if (line[i] == '\'')
+			i = s_quote(line, changed, i);
+		else if (line[i] == '$')
+			i = env_name(line, changed, i);
+		else if (line[i] == '<' || line[i] == '>')
+			i = redir_name(line, changed, i);
+		else if (line[i] == '|') 
+			changed[i] = 8;
 		else 
 			changed[i] = 9;
 		i++;
@@ -58,6 +118,24 @@ int	main(int ac, char **av)
 	printf("fin?\n");
 	i = 0;
 	while (i < length)
-		printf("%d ", changed[i++]);
+	{
+		printf("%c : %d\n", line[i], changed[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*line;
+
+	if (argc != 1 || !argv || !envp)
+		return (0);
+	printf("test1\n");
+	line = "hello \"inyang\". I`m \'ylee\'. good bye~ $PWD";
+	parsing(line);
+	printf("test2\n");
+	line = "echo \'$PWD is here\' and \"$PWD is here\" | cat << ylee | wc -l";
+	parsing(line);
 	return (0);
 }
